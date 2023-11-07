3fffe92fbff5173d4224c35db24fc5b5d4dd52f6
alim1381 <ali.moaydi2020@gmail.com>
2023-11-07 18:29:16 +0330

    edit nodes

---
+++ blockchainـobject.py
@@ -0,0 +1,109 @@
+import hashlib
+import json
+import requests
+from time import time
+from urllib.parse import urlparse
+
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [{
+            "index": 1,
+            "timestamp": 1698951286.4832501,
+            "trxs": [],
+            "proof": 0,
+            "previous_hash": ""
+        }]
+        self.current_trxs = []
+        self.nodes = set()
+        self.client_id = time()
+        self.nodes.add('127.0.0.1:5000')
+
+    def new_trx(self, sender, recipient, amount):
+        self.current_trxs.append({
+            "sender": sender,
+            "recipient": recipient,
+            "amount": amount,
+        })
+        return self.last_block["index"] + 1
+
+    def create_block(self):
+        proof = 1
+        endProsses = False
+        ts = time()
+        while endProsses is False:
+            block = {
+                "index": len(self.chain) + 1,
+                "timestamp": ts,
+                "trxs": [*self.current_trxs, {"sender": "", "recipient": self.client_id, "amount": 20, }],
+                "proof": proof,
+                "previous_hash": self.hash(self.chain[-1])
+            }
+            if self.valid_proof(block):
+                self.current_trxs = []
+                self.chain.append(block)
+                return block
+            else:
+                proof = proof + 1
+
+    def add_node(self, address):
+        parsed_node = urlparse(address)
+        self.nodes.add(parsed_node.netloc)
+
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+            if self.valid_proof(block) == False:
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def consensus(slef):
+        nodes = slef.nodes
+        new_chain = None
+        max_length = len(slef.chain)
+
+        for node in nodes:
+            res = requests.get(f'http://{node}/chain')
+            if res.status_code == 200:
+                length = res.json()['length']
+                chain = res.json()['chain']
+                print(chain)
+            if length > max_length and slef.valid_chain(chain):
+                new_chain = chain
+                max_length = length
+
+        if new_chain:
+            slef.chain = new_chain
+            return True
+
+        return False
+
+    def valid_proof(self, block):
+        block_hash = self.hash(block)
+        return block_hash[:4] == "0000"
+
+    def update_trx_list(self, block_trxs):
+        if len(self.current_trxs) > 0:
+            for t in self.current_trxs:
+                for bt in block_trxs:
+                    if t == bt:
+                        self.current_trxs.remove(t)
+        return True
+
+    @staticmethod
+    def hash(block):
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
--- ttc.py
+++ ttc.py
@@ -4,115 +4,11 @@ import requests
 from time import time
 from urllib.parse import urlparse
 from flask import Flask, jsonify, request
-
-
-class Blockchain:
-    def __init__(self):
-        self.chain = [{
-            "index": 1,
-            "timestamp": 1698951286.4832501,
-            "trxs": [],
-            "proof": 0,
-            "previous_hash": ""
-        }]
-        self.current_trxs = []
-        self.nodes = set()
-        self.client_id = time()
-
-    def new_trx(self, sender, recipient, amount):
-        self.current_trxs.append({
-            "sender": sender,
-            "recipient": recipient,
-            "amount": amount,
-        })
-        return self.last_block["index"] + 1
-
-    def create_block(self):
-        proof = 1
-        endProsses = False
-        ts = time()
-        while endProsses is False:
-            block = {
-                "index": len(self.chain) + 1,
-                "timestamp": ts,
-                "trxs": [*self.current_trxs, {"sender": "", "recipient": self.client_id, "amount": 20, }],
-                "proof": proof,
-                "previous_hash": self.hash(self.chain[-1])
-            }
-            if self.valid_proof(block):
-                self.current_trxs = []
-                self.chain.append(block)
-                return block
-            else:
-                proof = proof + 1
-
-    def add_node(self, address):
-        parsed_node = urlparse(address)
-        self.nodes.add(parsed_node.netloc)
-
-    def valid_chain(self, chain):
-        last_block = chain[0]
-        current_index = 1
-
-        while current_index < len(chain):
-            block = chain[current_index]
-            if block['previous_hash'] != self.hash(last_block):
-                return False
-            if self.valid_proof(block) == False:
-                return False
-
-            last_block = block
-            current_index += 1
-
-        return True
-
-    def consensus(slef):
-        nodes = slef.nodes
-        new_chain = None
-        max_length = len(slef.chain)
-
-        for node in nodes:
-            res = requests.get(f'http://{node}/chain')
-            if res.status_code == 200:
-                length = res.json()['length']
-                chain = res.json()['chain']
-                print(chain)
-            if length > max_length and slef.valid_chain(chain):
-                new_chain = chain
-                max_length = length
-
-        if new_chain:
-            slef.chain = new_chain
-            return True
-
-        return False
-
-    def valid_proof(self, block):
-        block_hash = self.hash(block)
-        return block_hash[:4] == "0000"
-
-    def update_trx_list(self, block_trxs):
-        if len(self.current_trxs) > 0:
-            for t in self.current_trxs:
-                for bt in block_trxs:
-                    if t == bt:
-                        self.current_trxs.remove(t)
-        return True
-
-    @staticmethod
-    def hash(block):
-        block_string = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(block_string).hexdigest()
-
-    @property
-    def last_block(self):
-        return self.chain[-1]
-
+from blockchainـobject import Blockchain
 
 blockchain = Blockchain()
 
 app = Flask(__name__)
-
 
 @app.route("/trx/new", methods=["POST"])
 def create_trx():
@@ -154,13 +50,21 @@ def register_node():
 
     # send a request for add address in nodes list
     address = request.get_json()
-    response = requests.post(
-        "http://127.0.0.1:5000/nodes/add", json={"address": address['node']})
+    nodes = blockchain.nodes
+
+    last_node_list = blockchain.nodes
+    max_length = len(last_node_list)
+    for n in nodes :
+        response = requests.post(f"http://{n}/nodes/add", json={"address": address['node']})
+        if response.status_code == 201:
+            print(len(response.json()['nodes']) > max_length)
+            print(response.json()['nodes'])
+            if len(response.json()['nodes']) > max_length :
+                last_node_list = response.json()['nodes']
 
     # if response ok add add received nodes to the list of nodes
-    if response.status_code == 201:
-        for node in response.json()['nodes']:
-            blockchain.nodes.add(node)
+    for node in last_node_list:
+        blockchain.nodes.add(node)
 
     res = {
         "message": "New nodes added successfully",
@@ -213,4 +117,4 @@ if __name__ == "__main__":
 
 
 # nodes ok
-# mineing
+# bug node downing
